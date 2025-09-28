bb68520ef574e093253a1a6872c4828290ac45fd
Wenhui Kuang <129364024+K2blackcat@users.noreply.github.com>
2025-09-28 16:17:04 -0700

    Session 2 Prompt For Code Journal #1

---
+++ 2.py
@@ -0,0 +1,20 @@
+# Sum of two floating point numbers
+a = 1.1
+b = 2.2
+summ = a+b
+print ("Sum:",summ, type(summ))
+
+#difference between two integers, 
+x = 77
+y = 11
+
+difference = x-y
+print("Difference:", difference, type(difference))
+
+
+#product of a floating point number and an integer.
+m = 5.5
+n = 10
+product = m * n
+
+print("Product:",product, type(product))
