cdcccd068a713ac8d973b66ae55fbd1d55985fbc
Wenhui Kuang <129364024+K2blackcat@users.noreply.github.com>
2025-10-02 11:51:06 -0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,4 +1,8 @@
-print("Full Name: Wenhui Kuang")
-print("Pronouns: he/his")
-print("My favorite movie is 'Rush hour' I really like the comedic elements in the movie. And Jackie Chan is my favorite actor.")
-print("My favorite food is dumplings, I am an Asian and tend to prefer Asian-style foods.")
+def introdution():
+
+	print("Full Name: Wenhui Kuang")
+	print("Pronouns: he/his")
+	print("My favorite movie is 'Rush hour' I really like the comedic elements in the movie. And Jackie Chan is my favorite actor.")
+	print("My favorite food is dumplings, I am an Asian and tend to prefer Asian-style foods.")
+
+introdution()
